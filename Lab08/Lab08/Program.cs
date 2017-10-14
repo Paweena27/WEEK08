68645911b2344565670fd87408f3126e1b2a5494
Paweena27 <59030185@kmitl.ac.th>
2017-10-14 23:11:17 +0700

    W08P05

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -7,6 +7,15 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
+            Console.WriteLine("Length of data = {0}", data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            Console.ReadLine();
+
+
         }
     }
 }

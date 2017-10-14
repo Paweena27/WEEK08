bbb87cdfd8fe5c53760b1dbd3cd0a1b97efe1636
Paweena27 <59030185@kmitl.ac.th>
2017-10-14 23:04:12 +0700

    W08P04

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -7,6 +7,21 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            int[] data = new int[10];
+            Student[] student = new Student[31];
+            Console.WriteLine(data.Length);
+            Console.WriteLine(student.Length);
+            Console.ReadLine();
+        }
+        class Student     // nested class
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+
+            }
         }
     }
 }

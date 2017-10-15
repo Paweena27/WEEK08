879acf55e083bc140e045c06024f66598cb7c7f5
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:33:49 +0700

    W08P31

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 
 namespace Lab08
@@ -7,6 +9,24 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(10);
+            stack1.Push(20);
+            stack1.Push(30);
+            stack1.Push(40);
+            Console.WriteLine("The stack elements are:");
+            foreach (int i in stack1)
+            {
+                Console.WriteLine(i);
+            }
+            ArrayList array = new ArrayList(stack1.ToArray());
+            Console.WriteLine("\nContents of the copy");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
         }
     }
 }

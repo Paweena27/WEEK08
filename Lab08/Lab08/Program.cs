011362bf70647e6dd13f441c29e91e2e96f122b5
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:19:48 +0700

    W08P26

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +7,17 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Queue<string> queue1 = new Queue<string>();
+            queue1.Enqueue("RED");
+            queue1.Enqueue("BLUE");
+            queue1.Enqueue("YELLOW");
+            queue1.Enqueue("GREEN");
+
+            Console.WriteLine("The elements in the queue are:" + queue1.Count);
+            queue1.Clear();
+            Console.WriteLine("The elements in the queue are after the clearmethod: " + queue1.Count);
+            Console.ReadLine();
+
         }
     }
 }

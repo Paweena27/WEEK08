156df5a0e38ff7e92340c7b24dd11767f4c2734f
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 19:24:16 +0700

    W08P20

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -7,6 +7,13 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            string[][] jagged = new string[2][];
+            jagged[0] = new string[] { "Europe", "Asia", "North America", "South America", "Africa", "Oceania", "Antarctica" };
+            jagged[1] = new string[] { "Greece", "	Ghana" };
+
+            Console.WriteLine(" List_names of countries in the world : " + (string.Join(",", jagged[0])));
+            Console.WriteLine("capital_names of of countries in the world : " + (string.Join(",", jagged[1])));
+            Console.ReadKey();
         }
     }
 }

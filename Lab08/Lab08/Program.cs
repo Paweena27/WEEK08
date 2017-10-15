b6d4e83a171f5d2e4970b72d91f3fabd99b616d9
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:40:06 +0700

    W08P32

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +8,30 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            dict1.Add("One", 1);
+            dict1.Add("Two", 2);
+            dict1.Add("Three", 3);
+            dict1.Add("Four", 4);
+            dict1.Add("Five", 5);
+            dict1.Add("Six", 6);
+            dict1.Add("Seven", 7);
+            dict1.Add("Eight", 8);
+            dict1.Add("Nine", 9);
+            dict1.Add("Ten", 10);
+
+            foreach (var item in dict1)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in dict1)
+            {
+                Console.WriteLine("key = {0}, value = {1}", item.Key, item.Value);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }

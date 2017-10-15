81343b5c9069a8457fd58e2ec93ecaf03feb1966
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:03:42 +0700

    W08P24

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +7,44 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Queue<int> q = new Queue<int>();
+            Console.WriteLine("-- enqueue 3 items -- ");
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- enqueue more items -- ");
+            q.Enqueue(40);
+            q.Enqueue(50);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- dequeue -- ");
+            int i = q.Dequeue();
+            Console.WriteLine("i = {0}", i);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+            Console.ReadLine();
+
         }
     }
 }

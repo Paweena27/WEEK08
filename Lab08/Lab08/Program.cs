1547d7fa1297f0f8066daf181e85d4e3bb7b562e
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:51:14 +0700

    W08P34

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +8,34 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> country = new Dictionary<string, string>();
+            country.Add("AF", "Afghanistan");
+            country.Add("AL", "Albania");
+            country.Add("AS", "American Samoa");
+            country.Add("AD", "Andorra");
+            country.Add("AM", "Armenia");
+            country.Add("AW", "Aruba");
+            country.Add("AU", "Australia");
+            country.Add("AT", "Austria");
+            country.Add("AZ", "Azerbaijan");
+            foreach (var item in country)
+            {
+                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
+            }
+            Console.WriteLine("Count of items = {0}", country.Count);
+            Console.WriteLine("\n** Direct access to value by key **");
+            country["AU"] = "AUSTRALIA";
+            Console.WriteLine("AU = " + country["AU"]);
+
+            Console.WriteLine("\n** Remove by key **");
+            country.Remove("AU");
+            foreach (var item in country)
+            {
+                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
+            }
+            Console.WriteLine("Count of items = {0}", country.Count);
+            Console.ReadLine();
+
         }
     }
 }

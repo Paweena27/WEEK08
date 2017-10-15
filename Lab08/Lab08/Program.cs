1b788c8ce3371c2f7c4481fc119a99cabad7cb86
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:08:39 +0700

    W08P25

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +7,27 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Queue<string> queue1 = new Queue<string>();
+            queue1.Enqueue("Red");
+            queue1.Enqueue("Green");
+            queue1.Enqueue("Blue");
+            queue1.Enqueue("Orange");
+            queue1.Enqueue("Yellow");
+            Console.WriteLine("The elements in the queue are:");
+            foreach (string s in queue1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("The element Red is contain in the queue:"
+ + queue1.Contains("Red"));
+            Console.WriteLine("The element Black is contain in the queue:"
+ + queue1.Contains("Black"));
+            Console.WriteLine("The element Purple is contain in the queue:"
+ + queue1.Contains("Purple"));
+            Console.WriteLine("The element Green is contain in the queue:"
+ + queue1.Contains("Green"));
+            Console.ReadLine();
+
         }
     }
 }

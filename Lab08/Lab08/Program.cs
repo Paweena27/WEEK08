c3d146d9805741f99edfb316753d7ca8f0ec5c02
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:46:18 +0700

    W08P33

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 
 namespace Lab08
@@ -7,6 +9,29 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> dict1 = new Dictionary<string, int>();
+            dict1.Add("One", 1);
+            dict1.Add("Two", 2);
+            dict1.Add("Three", 3);
+            dict1.Add("Four", 4);
+            dict1.Add("Five", 5);
+            dict1.Add("Six", 6);
+            dict1.Add("Seven", 7);
+            dict1.Add("Eight", 8);
+            dict1.Add("Nine", 9);
+            dict1.Add("Ten", 10);
+            if (dict1.ContainsKey("One"))
+            {
+                int value = dict1["One"];
+                Console.WriteLine(value);
+            }
+            if (dict1.ContainsKey("Eleven"))
+            {
+                int value = dict1["Eleven"];
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+
         }
     }
 }

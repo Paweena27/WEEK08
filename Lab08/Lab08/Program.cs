e87615cca91310da315911ce4dc3af41c699db73
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:21:48 +0700

    W08P27

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +8,24 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(10);
+            queue1.Enqueue(20);
+            queue1.Enqueue(30);
+            queue1.Enqueue(40);
+            Console.WriteLine("The queue elements are:");
+            foreach (int i in queue1)
+            {
+                Console.WriteLine(i);
+            }
+            ArrayList array = new ArrayList(queue1.ToArray());
+            Console.WriteLine("\nContents of the copy");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
         }
     }
 }

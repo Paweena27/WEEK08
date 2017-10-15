b57a34990850887e5b4b1bb147009a6d6eafb1ff
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 19:59:06 +0700

    W08P23

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections;
 
 namespace Lab08
 {
@@ -7,6 +7,26 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            ArrayList list1 = new ArrayList();
+            list1.Add(10);
+            list1.Add(20);
+            list1.Add(30);
+            list1.Add(40);
+            for (int i = 0; i < list1.Count; i++)
+            {
+                Console.WriteLine(list1[i]);
+            }
+            ArrayList list2 = new ArrayList(list1);
+            list2[2] = 55;
+            list2[3] = 66;
+            list2[4] = 77;
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                Console.WriteLine(list2[i]);
+            }
+            Console.ReadLine();
+
         }
     }
 }

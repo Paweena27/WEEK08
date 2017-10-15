9fcf63d1001e1e3e7477163f8f6a13c1f8a47b69
Paweena27 <59030185@kmitl.ac.th>
2017-10-15 20:31:15 +0700

    W08P30

--- Lab08/Lab08/Program.cs
+++ Lab08/Lab08/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace Lab08
 {
@@ -7,6 +7,24 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
+            Stack<string> stack1 = new Stack<string>();
+            stack1.Push("************");
+            stack1.Push("RED");
+            stack1.Push("GREEN");
+            stack1.Push("BLUE");
+            stack1.Push("YELLOW");
+            stack1.Push("***********");
+            stack1.Push("** COLOR **");
+            stack1.Push("***********");
+            Console.WriteLine("The elements in the stack1 are as:");
+            foreach (string s in stack1)
+            {
+                Console.WriteLine(s);
+            }
+            stack1.Clear();
+            Console.WriteLine("After apply the clear method the elements in thestack are: " + stack1.Count);
+            Console.ReadLine();
+
         }
     }
 }
